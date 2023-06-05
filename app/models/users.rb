b75d79f4439db27bb099c98e8c1f6dae4537feca
Job Muturi <muirimuturi17@gmail.com>
2023-06-05 08:13:09 +0300

    Object Association Methods - Users

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+
+    def self.reviews
+      Review.find(self.user_id)
+    end
+    def self.products
+      Product.find(self.user_id)
+    end
   end
